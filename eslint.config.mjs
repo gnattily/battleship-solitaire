f9f30b69d19c9fd8d0fbdbddfc6913c0f2879666
gnattily <93106270+gnattily@users.noreply.github.com>
2025-06-29 12:27:35 -0500

    added jsdoc linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import stylistic from '@stylistic/eslint-plugin';
 import jsoncParser from 'jsonc-eslint-parser';
 import pluginJsonc from 'eslint-plugin-jsonc';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,8 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
+
+const allScripts = ['**/*.{js,jsx,mjs,ts,tsx,mts}'];
 
 const stylisticConfig = stylistic.configs.customize({
     braceStyle: '1tbs',
@@ -22,15 +25,29 @@ const stylisticConfig = stylistic.configs.customize({
 
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals'),
+    jsdoc.configs['flat/recommended'],
 
     {
-        files: ['**/*.{js,jsx,mjs,ts,tsx}'],
+        files: allScripts,
         ...stylisticConfig,
         rules: {
             ...stylisticConfig.rules,
             '@stylistic/max-statements-per-line': 'off',
             '@stylistic/space-before-function-paren': ['error', 'always'],
             '@stylistic/arrow-parens': ['error', 'as-needed', { requireForBlockBody: false }],
+        },
+    },
+
+    {
+        files: allScripts,
+        plugins: {
+            jsdoc,
+        },
+        rules: {
+            'jsdoc/sort-tags': 'warn',
+            'jsdoc/require-jsdoc': 'off',
+            'jsdoc/require-hyphen-before-param-description': 'warn',
+            'jsdoc/no-defaults': 'off',
         },
     },
 
